7962ccfeb6d3b588ca454ccde4542d212f465982
Jason Freeberg <jafreebe@microsoft.com>
2020-09-17 17:43:23 -0700

    Update SpringTestDemoApplication.java

--- src/main/java/com/azure/appservice/springtestdemo/SpringTestDemoApplication.java
+++ src/main/java/com/azure/appservice/springtestdemo/SpringTestDemoApplication.java
@@ -2,7 +2,7 @@ package com.azure.appservice.springtestdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
